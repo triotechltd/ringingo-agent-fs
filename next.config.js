893cd77af8616e712fb155bdc41b6231362861da
Ubaidtriotech1999 <ubaid.khan@triotech.in>
2025-12-09 17:58:07 +0530

    feat: migrate image icons to SVG React icon system

--- next.config.js
+++ next.config.js
@@ -1,27 +1,32 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: process.env.ANALYZE === "true",
 // });
 
 const nextConfig = {
-  /* config options here */
-  env: {
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL_APP,
-    TENANT_PORTAL_DOMAIN: process.env.NEXT_PUBLIC_TENANT_PORTAL_DOMAIN,
-    SIDEPANEL_1: process.env.NEXT_PUBLIC_SIDEPANEL_1,
-    SIDEPANEL_2: process.env.NEXT_PUBLIC_SIDEPANEL_2,
-    SIDEPANEL_3: process.env.NEXT_PUBLIC_SIDEPANEL_3,
-    FOOTER: process.env.NEXT_PUBLIC_FOOTER,
-    TITLE: process.env.NEXT_PUBLIC_TITLE,
-    DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION,
-    LOGIN: process.env.NEXT_PUBLIC_LOGIN,
-    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    WSS_URL: process.env.NEXT_PUBLIC_WSS_URL,
-    CHAT_SOCKET_URL: process.env.NEXT_PUBLIC_CHAT_SOCKET_URL,
-    EXPANDED_LOGO: process.env.NEXT_PUBLIC_EXPANDED_LOGO,
-    COLLAPSED_LOGO: process.env.NEXT_PUBLIC_COLLAPSED_LOGO,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL_TENANT,
+    TENANT_PORTAL_DOMAIN:  "10.180.67.147",
+    SIDEPANEL_1: process.env.SIDEPANEL_1 || "Your Unified",
+    SIDEPANEL_2: process.env.SIDEPANEL_2 || "Communications Platform.",
+    SIDEPANEL_3: process.env.SIDEPANEL_3 || "Smart Contact Center platform for High performance teams.",
+    FOOTER: process.env.FOOTER || "CALL CENTER, All rights reserved.",
+    TITLE: process.env.TITLE || "CALL CENTER",
+    DESCRIPTION: process.env.DESCRIPTION || "Smart Contact Center platform for High performance teams.",
+    EXPANDED_LOGO: process.env.EXPANDED_LOGO || "/assets/images/ringingo_expanded_logo.svg",
+    COLLAPSED_LOGO: process.env.COLLAPSED_LOGO || "/assets/images/ringingo_collapsed_logo.svg",
+    BASE_URL:"https://devapiapp.ringingo.com",
+    WSS_URL: "wss://devapiapp.ringingo.com:7443",
+    // BASE_URL: "https://localhost:5000",
+  },
 };
 
 module.exports = nextConfig;
